c13e34530bc9bfb63a848b52ed894632711a176d
Domingos31 <djcgp@iscte-iul.pt>
2025-06-06 12:38:53 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,9 @@
-# from flask import Flask
-# from flask import Flask, render_template
-from Flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
-# app.secret_key = 'sua_chave_secreta'  # Necessário para flash messages
 app.secret_key = '24e23c43d423c434343vfghfgd'
 
-
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -19,17 +15,17 @@ def login():
             flash('Senha deve ter 6+ caracteres!', 'error')
         elif password != "123456":
             flash('Senha incorreta!', 'error')
-        # simular acesso a database
+        # Simular acesso a database
         elif username == "admin" and password == "123456":
             return redirect(url_for('dashboard'))
         else:
             flash('Credenciais inválidas!', 'error')
-
+    
     return render_template('login.html')
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # se tiveres uma página principal
+    return render_template('index.html')
 
 @app.route('/registar')
 def registar():
@@ -39,34 +35,23 @@ def registar():
 def recuperar_password():
     return render_template('recuperar-password.html')
 
-
 @app.route('/dashboard')
 def dashboard():
-    # return "Página restrita - Dashboard"
     return render_template('index.html')
-    
 
 @app.route('/user/<username>')
 def show_user(username):
     return f"Bem-vindo, {username}!"
 
-
 @app.route("/sobre")
 def sobre():
     return render_template('sobre.html')
-
 
 @app.route('/busca', methods=['GET'])
 def busca():
     termo = request.args.get('q')
     return f"Você buscou por: {termo}"
 
-
-
-
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=8000)
     app.run(debug=True)
-
-
     
